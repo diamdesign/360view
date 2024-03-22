35bc31fd8ca9fb6ea8084cf7ea47ead506c47351
Fredrik Berglund <fredrik@diam.se>
2024-03-22 20:20:29 +0100

    Fix css

--- components/leftmenu.php
+++ components/leftmenu.php
@@ -1,10 +1,13 @@
 <div id="leftmenu">
     <div id="toggle-leftpanel"></div>
     <ul style="list-style:inside;font-size:0.8rem;padding:1rem;">
-        <li>Fix the comments</li>
-        <li>Fix the emoji for comments</li>
-        <li>Fix css for login/signup</li>
+        <li>Fix the reply and like eventlistener in comments</li>
+        <li>Fix Enter to send comment into database</li>
+        <li>Get the share section done</li>
         <li>Fix the captcha plugin for signup</li>
         <li>Fix login and signup inside the 360</li>
+        <li>Fix the like button for location</li>
+        <li>Add like button for project</li>
+        <li>Add follow creator button</li>
     </ul>
 </div> 
